f21727af95cfefbd8e59e498c6c1572c85bf1e1e
SmithCray <cmsmith004@gmail.com>
2021-11-14 18:23:43 -0700

    TimeDate_added/displayFormat_toPage

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,23 @@
 //JS text
 console.log("this");
+
+const clock = document.getElementById("currentDay");
+
+function updateTime() {
+  const now = moment();
+  const displayTime = now.format("MMMM Do YYYY, h:mm:ss a");
+
+  clock.textContent = displayTime;
+}
+setInterval(updateTime, 1000);
+
+// var currentTime = moment();
+// console.log(currentTime);
+
+// function displayTime() {
+//   document.getElementById("#currentDay") = "Today is " + currentTime;
+// }
+
+// var m = moment.utc("");
+
+// console.log(m);
