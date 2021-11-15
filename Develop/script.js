583ba23b61fe6c02c6725e8bd92407f0b01121f3
SmithCray <cmsmith004@gmail.com>
2021-11-14 23:18:36 -0700

    Final_layout/Bug_testing/workingOn_functions

--- Develop/script.js
+++ Develop/script.js
@@ -2,12 +2,35 @@
 console.log("this");
 
 const clock = document.getElementById("currentDay");
+const row = document.getElementById("hourRow");
+var timeBlock = moment().hour();
+console.log(timeBlock);
+
+var task = document.querySelector("task");
+var saveButton = document.querySelector("saveBtn");
+var data = localStorage.getItem("data");
+
+task.textContent = data;
+
+saveButton.addEventListener("click", function () {
+  if (data >= "1") {
+    task.textContent = data;
+    localStorage.setItem("data", data);
+  }
+});
 
 function updateTime() {
   const now = moment();
-  const displayTime = now.format("MMMM Do YYYY, h:mm:ss a");
-
+  const displayTime = now.format("MMMM Do YYYY, h:mm: a");
   clock.textContent = displayTime;
+
+  if (timeBlock === document.getElementById("hourRow")) {
+    row.classlist.add("present");
+  } else if (timeBlock > document.getElementById("hourRow")) {
+    row.classlist.add("future");
+  } else if (timeBlock < document.getElementById("hourRow")) {
+    row.classlist.add("past");
+  }
 }
 setInterval(updateTime, 1000);
 
